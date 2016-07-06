03bc6427f2b793f51bd4362aea2749d73051f202
Lana-chan <erin.pinheiro@usp.br>
2016-07-06 20:23:54 -0300

    ordem certa

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var $header = $('#menu');
 $().ready(function () {
 	$("#video-img").click(function (){
    	var div = '<div id="video-embed"><iframe src="https://www.youtube.com/embed/oliAYl6853w?autoplay=1" frameborder="0" allowfullscreen></iframe></div>';
-	  $(div).insertAfter(this);
+	  $(div).insertBefore(this);
 		$(this).fadeOut("slow");
   });
   
