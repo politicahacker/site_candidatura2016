f8b1ce07c2f54779c0307ff560e441f449473641
Lana-chan <erin.pinheiro@usp.br>
2016-07-06 19:20:04 -0300

    smooth scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
 // Empty JS for your own code to be here
+var $root = $('html, body');
+var $header = $('#menu');
+
 $().ready(function () {
 	$("#video-img").click(function (){
 		$('#video-img').fadeOut("slow", function(){
     	var div = $('');
     	$(this).replaceWith(div);
-
-	});
-});
+	  });
+  });
+  
+  $('a[href*=#]').click(function() {
+    $root.animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top - $header.height()
+    }, 500);
+    return false;
+  });
 })
