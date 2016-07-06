4d2287f6191c31c4389f4ae13cc7704b47ab6f78
Pedro Markun <pedro@markun.com.br>
2016-07-06 17:58:20 -0300

    mais fixes, video responsivo da erin

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 // Empty JS for your own code to be here
+$().ready(function () {
+	$("#video-img").click(function (){
+		$('#video-img').fadeOut("slow", function(){
+    	var div = $('');
+    	$(this).replaceWith(div);
+
+	});
+});
+})
