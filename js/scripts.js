b8248f2fea3269c0227784b9cddcee00d3cb40f8
Pedro Markun <pedro@markun.com.br>
2016-07-07 10:11:55 -0300

    adicionando codigo do analytics

--- js/scripts.js
+++ js/scripts.js
@@ -21,4 +21,28 @@ $().ready(function () {
   	$(this).hide();
   	$("#videos .more").fadeIn("slow");
   });
+
+
+//GAnalytics
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-80435506-1', 'auto');
+  ga('send', 'pageview');
+  
+  //Timeout event
+  setTimeout("ga('send', 'event', { eventCategory: '30 seconds on page', eventAction: 'Read' })", 30000);
+  
+  //Scroll event  
+  var runned = false; 
+  $( window ).scroll( function() { 
+  if ( $( document ).scrollTop() >= 250 && runned == false ) { 
+  ga('send', 'event', { eventCategory: 'Scrolled 250px', eventAction: 'Scroll' }); 
+  runned = true; 
+  }});
+
 })
+
+
